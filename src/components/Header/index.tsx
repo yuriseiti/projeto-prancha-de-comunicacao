2347a72d9f99598b12531e3a402c58581a8d10ac
Yuri Seiti <yuriseiti@gmail.com>
2024-12-02 19:23:50 -0400

    add header text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { HeaderContainer } from "./styles";
 import Logo from "../../assets/semed-logo.png";
+import { Typography, Box } from "@mui/material";
 
 const Header: React.FC = () => {
-
   return (
     <HeaderContainer>
-      <img
-        src={Logo}
-        alt="Logo"
-        style={{ height: "80px", cursor: "pointer" }}
-        onClick={() => window.location.reload()}
-      />
+      <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+        <img
+          src={Logo}
+          alt="Logo"
+          style={{ height: "80px", cursor: "pointer", marginRight: "16px" }}
+          onClick={() => window.location.reload()}
+        />
+        <Typography
+          sx={{
+            fontSize: "1.7rem",
+            fontWeight: "bold",
+            color: "#fff",
+            textAlign: "center",
+            flexGrow: 1,
+            marginRight: "80px",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          Centro Municipal de Educação Inclusiva da REME
+        </Typography>
+      </Box>
     </HeaderContainer>
   );
 };
