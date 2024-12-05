074d765788184b6ec77f41c11cd7dcc685380aa2
Yuri Seiti <yuriseiti@gmail.com>
2024-12-05 09:43:19 -0400

    revert pictograms

--- src/assets/Pictograms/index.ts
+++ src/assets/Pictograms/index.ts
@@ -1,6 +1,6 @@
 // src/assets/Pictograms/index.ts
 
-// import oi from "./oi.png";
+import oi from "./oi.png";
 import eu from "./eu.png";
 import voce from "./voce.png";
 import quero from "./quero.jpeg";
@@ -9,67 +9,67 @@ import agua from "./agua.png";
 import banheiro from "./banheiro.png";
 import sair from "./sair.png";
 import comer from "./comer.png";
-// import conversar from "./conversar.png";
-// import triste from "./triste.png";
-// import dor from "./dor.png";
-// import feliz from "./feliz.png";
+import conversar from "./conversar.png";
+import triste from "./triste.png";
+import dor from "./dor.png";
+import feliz from "./feliz.png";
 import nao from "./nao.png";
 import sim from "./sim.png";
-// import lavarAsMaos from "./lavar_as_maos.png";
-// import elevador from "./elevador.png";
-// import escada from "./escada.png";
-// import acabou from "./acabou.png";
-// import dormir from "./dormir.png";
-// import obrigado from "./obrigado.png";
-// import tchau from "./tchau.png";
-// import atividade from "./atividade.png";
+import lavarAsMaos from "./lavar_as_maos.png";
+import elevador from "./elevador.png";
+import escada from "./escada.png";
+import acabou from "./acabou.png";
+import dormir from "./dormir.png";
+import obrigado from "./obrigado.png";
+import tchau from "./tchau.png";
+import atividade from "./atividade.png";
 import brincar from "./brincar.png";
-// import parque from "./parque.png";
-// import esperar from "./esperar.png";
+import parque from "./parque.png";
+import esperar from "./esperar.png";
 import sentar from "./sentar.png";
-// import massinha from "./massinha.png";
-// import abafador from "./abafador.png";
-// import onibus from "./onibus.png";
-// import psicomotricidade from "./psicomotricidade.jpeg";
-// import arte from "./arte.jpeg";
-// import musica from "./musica.png";
-// import linguagem from "./linguagem.jpeg";
-// import logicamatematica from "./logicamatematica.jpeg";
+import massinha from "./massinha.png";
+import abafador from "./abafador.png";
+import onibus from "./onibus.png";
+import psicomotricidade from "./psicomotricidade.jpeg";
+import arte from "./arte.jpeg";
+import musica from "./musica.png";
+import linguagem from "./linguagem.jpeg";
+import logicamatematica from "./logicamatematica.jpeg";
 
 export const PictogramLabels = [
-  // { image: oi, text: "Oi" },
-  // { image: tchau, text: "Tchau" },
+  { image: oi, text: "Oi" },
+  { image: tchau, text: "Tchau" },
   { image: eu, text: "Eu" },
   { image: voce, text: "Você" },
   { image: quero, text: "Quero" },
   { image: naoQuero, text: "Não quero" },
   { image: agua, text: "Água" },
   { image: banheiro, text: "Banheiro" },
-  // { image: atividade, text: "Atividade" },
+  { image: atividade, text: "Atividade" },
   { image: brincar, text: "Brincar" },
-  // { image: massinha, text: "Massinha" },
-  // { image: abafador, text: "Abafador" },
-  // { image: parque, text: "Parque" },
-  // { image: esperar, text: "Esperar" },
+  { image: massinha, text: "Massinha" },
+  { image: abafador, text: "Abafador" },
+  { image: parque, text: "Parque" },
+  { image: esperar, text: "Esperar" },
   { image: sentar, text: "Sentar" },
-  // { image: elevador, text: "Elevador" },
-  // { image: escada, text: "Escada" },
-  // { image: conversar, text: "Conversar" },
-  // { image: lavarAsMaos, text: "Lavar as mãos" },
-  // { image: dormir, text: "Dormir" },
+  { image: elevador, text: "Elevador" },
+  { image: escada, text: "Escada" },
+  { image: conversar, text: "Conversar" },
+  { image: lavarAsMaos, text: "Lavar as mãos" },
+  { image: dormir, text: "Dormir" },
   { image: comer, text: "Comer" },
-  // { image: feliz, text: "Feliz" },
-  // { image: triste, text: "Triste" },
-  // { image: dor, text: "Dor" },
+  { image: feliz, text: "Feliz" },
+  { image: triste, text: "Triste" },
+  { image: dor, text: "Dor" },
   { image: sim, text: "Sim" },
   { image: nao, text: "Não" },
-  // { image: psicomotricidade, text: "Psicomotricidade" },
-  // { image: arte, text: "Arte" },
-  // { image: musica, text: "Música" },
-  // { image: linguagem, text: "Linguagem" },
-  // { image: logicamatematica, text: "Lógica Matemática" },
+  { image: psicomotricidade, text: "Psicomotricidade" },
+  { image: arte, text: "Arte" },
+  { image: musica, text: "Música" },
+  { image: linguagem, text: "Linguagem" },
+  { image: logicamatematica, text: "Lógica Matemática" },
   { image: sair, text: "Ir embora" },
-  // { image: onibus, text: "Ônibus" },
-  // { image: obrigado, text: "Obrigado" },
-  // { image: acabou, text: "Acabou" },
+  { image: onibus, text: "Ônibus" },
+  { image: obrigado, text: "Obrigado" },
+  { image: acabou, text: "Acabou" },
 ];
