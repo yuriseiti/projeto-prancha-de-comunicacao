42d76ebf30333b9f9bfa13d25adde44e3fcb5937
Yuri Seiti <yuriseiti@gmail.com>
2024-12-04 22:08:20 -0400

    fix text positioning

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -8,16 +8,12 @@ const Content: React.FC = () => {
 
   const speakText = (text: string, index: number) => {
     window.speechSynthesis.cancel();
-
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = "pt-BR";
-
     setSpeaking(index);
-
     utterance.onend = () => {
       setSpeaking(null);
     };
-
     window.speechSynthesis.speak(utterance);
   };
 
@@ -27,40 +23,37 @@ const Content: React.FC = () => {
         {PictogramLabels.map((pictogram, index) => (
           <Grid item xs={12} sm={6} md={4} xl={2} key={index}>
             <Box
-              onClick={() => speakText(pictogram.text, index)}
               sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                justifyContent: "center",
-                width: "100%",
-                paddingTop: "100%",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                border: "1px solid #ccc",
-                boxShadow: "0 2px 8px rgba(0,0,0,0.2)",
-                borderRadius: "8px",
-                backgroundImage: `url(${pictogram.image})`,
-                position: "relative",
-                cursor: "pointer",
-                transition: "transform 0.2s",
-                "&:hover": {
-                  transform: "scale(1.05)",
-                },
-                opacity: speaking === index ? 0.7 : 1,
               }}
             >
+              <Box
+                onClick={() => speakText(pictogram.text, index)}
+                sx={{
+                  width: "100%",
+                  paddingTop: "100%",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  border: "1px solid #ccc",
+                  boxShadow: "0 2px 8px rgba(0,0,0,0.2)",
+                  borderRadius: "8px",
+                  backgroundImage: `url(${pictogram.image})`,
+                  cursor: "pointer",
+                  transition: "transform 0.2s",
+                  "&:hover": {
+                    transform: "scale(1.05)",
+                  },
+                  opacity: speaking === index ? 0.7 : 1,
+                }}
+              />
               <Typography
                 variant="h6"
                 component="div"
                 sx={{
-                  position: "absolute",
-                  bottom: "0px",
                   color: "#000",
                   textAlign: "center",
-                  width: "calc(100% - 16px)",
-                  backgroundColor: "rgba(255,255,255,0.8)",
-                  borderRadius: "4px",
                   padding: "4px",
                 }}
               >
